fc209be619434373d375326c426738b6a212df13
Third-Flegm <ruben.palkovits@icloud.com>
2026-09-15 12:16:50 +0200

    Update and rename src/setup.py to setup.py

--- src/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="flegmgui",  # The name you will use to install it
-    version="0.1.0",
-    packages=find_packages(), # Automatically finds your inner folder
-)
