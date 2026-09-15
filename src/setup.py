83b6b01bf6ed7d4610009626c21eff92dc2065f1
Third-Flegm <ruben.palkovits@icloud.com>
2026-09-15 12:13:34 +0200

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flegmgui",  # The name you will use to install it
+    version="0.1.0",
+    packages=find_packages(), # Automatically finds your inner folder
+)
